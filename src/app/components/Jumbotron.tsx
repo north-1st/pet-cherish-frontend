906687cf3bdedec8d576968a0546073b49770e41
vita hsieh <dragic.hsieh@gmail.com>
2024-05-16 01:02:53 +0800

    feat: add search page link

--- src/app/components/Jumbotron.tsx
+++ src/app/components/Jumbotron.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 
@@ -17,12 +18,16 @@ const Jumbotron = () => {
           <h1 className='text-5xl font-medium tracking-tighter'>寵愛無界 - 為愛犬生活加點色彩</h1>
           <p className='text-xl/relaxed text-gray-500'>「專業服務,讓每一刻成為他們的快樂時光」</p>
           <div className='flex gap-4'>
-            <Button className='bg-orange-500 hover:bg-orange-600' variant='default'>
-              找保姆
-            </Button>
-            <Button className='bg-gray-900 text-white hover:bg-gray-800' variant='secondary'>
-              找任務
-            </Button>
+            <Link className='text-lg font-bold text-gray-900' href='/search/sitters'>
+              <Button className='bg-orange-500 hover:bg-orange-600' variant='default'>
+                找保姆
+              </Button>
+            </Link>
+            <Link className='text-lg font-bold text-gray-900' href='/search/tasks'>
+              <Button className='bg-gray-900 text-white hover:bg-gray-800' variant='secondary'>
+                找任務
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
