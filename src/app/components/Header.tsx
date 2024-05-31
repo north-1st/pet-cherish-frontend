9602198201e31711582e2acdc5ad392a1ddd9c2b
vita hsieh <dragic.hsieh@gmail.com>
2024-06-01 03:57:57 +0800

    feat: add router groups

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,8 @@
-'use client';
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 
 const Header = () => {
-  const pathname = usePathname();
-  const pathsWithoutHeaderAndFooter = ['/register', '/login'];
-  const shouldHideHeaderAndFooter = pathsWithoutHeaderAndFooter.includes(pathname);
-
-  if (shouldHideHeaderAndFooter) return null;
-
   return (
     <header className='bg-white'>
       <div className='container flex h-16 items-center justify-between'>
