08bd68f00187332972d75a82cfb192ded4308de5
vita hsieh <dragic.hsieh@gmail.com>
2024-06-02 00:39:25 +0800

    feat: adjust auth layout

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 import { Card, CardContent } from '@/components/ui/card';
@@ -7,36 +6,18 @@ import RegisterForm from './components/RegisterForm';
 
 const Register = () => {
   return (
-    <section className='flex min-h-screen items-center justify-center bg-[#F5F5F5]'>
-      <div className='flex'>
-        <Card className='w-[350px] rounded-none rounded-l-lg border-none bg-gradient-to-br from-[#89f7fe] to-[#66a6ff] shadow-none'>
-          <CardContent className='flex h-[100%] flex-col items-center justify-center p-10'>
-            <h2 className='mb-2 text-3xl font-bold'>Glad to see You!</h2>
-            <Link href='/'>
-              <Image
-                alt='Logo'
-                className='text-white'
-                height='44'
-                width='142'
-                src='/images/logo1.png'
-              />
-            </Link>
-          </CardContent>
-        </Card>
-        <Card className='w-[350px] rounded-none rounded-r-lg border-none shadow-none'>
-          <CardContent className='p-10'>
-            <h1 className='mb-4 text-center text-xl font-semibold'>Register</h1>
-            <RegisterForm />
-            <p className='mt-4 text-center text-sm'>
-              Already have an account?&nbsp;
-              <Link className='text-blue-500 hover:text-blue-600' href='/login'>
-                Log in
-              </Link>
-            </p>
-          </CardContent>
-        </Card>
-      </div>
-    </section>
+    <Card className='w-[350px] rounded-none rounded-r-lg border-none shadow-none'>
+      <CardContent className='p-10'>
+        <h1 className='mb-4 text-center text-xl font-semibold'>Register</h1>
+        <RegisterForm />
+        <p className='mt-4 text-center text-sm'>
+          Already have an account?&nbsp;
+          <Link className='text-blue-500 hover:text-blue-600' href='/login'>
+            Log in
+          </Link>
+        </p>
+      </CardContent>
+    </Card>
   );
 };
 
