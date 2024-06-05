6d3007d2fcb820cbe005e6da5d84e90739639475
Lucy <lucy70381@kimo.com>
2024-06-06 00:12:20 +0800

    refactor: clean up the width & height in icons

--- src/components/common/Icon/Female.tsx
+++ src/components/common/Icon/Female.tsx
@@ -2,11 +2,11 @@ import { IconProps } from '@/schemas/iconPropsSchema';
 
 import { cn } from '@/lib/utils';
 
-const FemaleIcon = ({ width, height, className }: IconProps) => {
+const FemaleIcon = ({ width = 24, height = 24, className }: IconProps) => {
   return (
     <svg
-      width={width ?? 24}
-      height={height ?? 24}
+      width={width}
+      height={height}
       viewBox='0 0 24 24'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
