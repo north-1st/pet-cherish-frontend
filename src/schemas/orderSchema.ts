64d18a9976624f2ce07713e8239e84e972097a4e
Joanna Chen <joannachenchencom@gmail.com>
2024-06-23 19:02:48 +0800

    feat: updatePaymentStatusOrder, paymentComplete

--- src/schemas/orderSchema.ts
+++ src/schemas/orderSchema.ts
@@ -1,4 +1,4 @@
-import { ZodObject, ZodRawShape, optional, z } from 'zod';
+import { ZodObject, ZodRawShape, z } from 'zod';
 
 import { paginationSchema } from './pagination';
 import { taskResponseSchema } from './taskSchema';
@@ -20,6 +20,17 @@ export const paymentStatusSchema = z.enum([
   'FAILURE', // 付款失敗
 ]);
 
+export const stripePaymentStatusSchema = z.enum(['paid', 'unpaid', 'no_payment_required']);
+
+export const paymentMethodTypeSchema = z.enum([
+  'card',
+  'bank_transfer',
+  'alipay',
+  'apple_pay',
+  'google_pay',
+  'wechat',
+]);
+
 export const createOrderBodySchema = z.object({
   task_id: z.string(),
   note: z.string(),
@@ -37,8 +48,9 @@ export const orderResponseSchema = z.object({
   note: z.string(),
   payment_id: z.string().nullable().optional(),
   payment_url: z.string().nullable().optional(),
-  payment_status: z.string().nullable().optional(),
+  payment_status: paymentStatusSchema.nullable().optional(),
   payment_at: z.string().nullable().optional(),
+  payment_type: paymentMethodTypeSchema.nullable().optional(),
   report_content: z.string(),
   report_image_list: z.array(z.string()),
   report_created_at: z
@@ -55,6 +67,13 @@ export const orderResponseSchema = z.object({
   task: taskResponseSchema,
 });
 
+export const updatePaymentStatusOrderBodySchema = z.object({
+  task_id: z.string(),
+  payment_at: z.number(),
+  payment_status: paymentStatusSchema,
+  payment_type: paymentMethodTypeSchema,
+});
+
 const productSchema = z.object({
   name: z.string(),
   price: z.number(),
@@ -65,7 +84,46 @@ export const checkoutBodyRequestSchema = z.object({
   products: z.array(productSchema),
   metadata: z.object({
     order_id: z.string(),
+    task_id: z.string(),
   }),
+});
+
+export const completeResponseBodySchema = z.object({
+  retrieve: z.object({
+    id: z.string(),
+    amount_total: z.number(),
+    metadata: z.object({
+      order_id: z.string(),
+      task_id: z.string(),
+    }),
+    payment_status: stripePaymentStatusSchema,
+    payment_intent: z.object({
+      amount: z.number(),
+      amount_received: z.number(),
+      created: z.number(), // 付款时间
+      payment_method: z.object({
+        type: paymentMethodTypeSchema,
+        card: z
+          .object({
+            brand: z.string(),
+            last4: z.string(),
+          })
+          .optional(),
+      }),
+    }),
+  }),
+  list_items: z.array(
+    z.object({
+      id: z.string(),
+      amount_total: z.number(),
+      currency: z.string(),
+      description: z.string(),
+      price: z.object({
+        unit_amount: z.number(),
+      }),
+      quantity: z.number(),
+    })
+  ),
 });
 
 export function createResponsePaginationDataSchema<T extends ZodRawShape>(
@@ -78,19 +136,23 @@ export function createResponsePaginationDataSchema<T extends ZodRawShape>(
     status: z.boolean(),
   });
 }
+
 export const ordersPaginationResponseSchema =
   createResponsePaginationDataSchema(orderResponseSchema);
 
-export function createBaseResponseDataSchema(dataSchema: z.AnyZodObject) {
+export function createBaseResponseDataSchema<T extends ZodRawShape>(dataSchema: ZodObject<T>) {
   return z.object({
     data: dataSchema,
     status: z.boolean(),
   });
 }
+
 export const orderDataResponseSchema = z.object({
   data: orderResponseSchema,
   status: z.boolean(),
 });
+
+export const completeResponseSchema = createBaseResponseDataSchema(completeResponseBodySchema);
 
 export const orderResponseListSchema = z.array(orderResponseSchema);
 
@@ -109,3 +171,7 @@ export type OrderPaginationResponse = z.infer<typeof ordersPaginationResponseSch
 export type PaymentStatus = z.infer<typeof paymentStatusSchema>;
 
 export type PaymentRequest = z.infer<typeof checkoutBodyRequestSchema>;
+
+export type UpdatePaymentStatusOrderRequest = z.infer<typeof updatePaymentStatusOrderBodySchema>;
+
+export type CompleteResponse = z.infer<typeof completeResponseSchema>;
