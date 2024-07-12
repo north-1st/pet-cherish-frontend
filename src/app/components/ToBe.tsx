8a7b5a733d37e388526a29e48bb2e9d8b6a525e7
Lucy <lucy70381@kimo.com>
2024-07-12 18:44:30 +0800

    style: update the UI of ToBe

--- src/app/components/ToBe.tsx
+++ src/app/components/ToBe.tsx
@@ -7,12 +7,13 @@ const ToBe = () => {
         <div className='group relative'>
           <Image
             alt='Woman with a dog'
-            className='aspect-[1739/437] h-auto w-full rounded-lg object-cover group-hover:scale-100'
+            className='h-[400px] w-full rounded-lg object-cover group-hover:scale-100'
             height='437'
             src='/images/tobe1.png'
             width='1739'
           />
-          <div className='absolute bottom-10 left-1/3 flex flex-col items-center'>
+          <div className='absolute inset-0 rounded-lg bg-gradient-to-b from-black/0 to-black/50' />
+          <div className='absolute bottom-6 left-0 right-0 mx-4 flex flex-col items-start md:bottom-10 md:items-center'>
             <h2 className='text-3xl font-bold text-white'>成為寵物保母</h2>
             <p className='mt-2 text-white'>
               您可以根據自己的時間表來安排工作,這樣就能夠平衡工作和個人生活。
